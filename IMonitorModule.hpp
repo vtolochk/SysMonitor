76be20706dc5a7091134333ec18e06d39bc7dc33
Valentin TOLOCHKO <vtolochk@e1r4p2.unit.ua>
2018-06-30 10:04:18 +0300

    new methods

--- IMonitorModule.hpp
+++ IMonitorModule.hpp
@@ -1,12 +1,17 @@
-#ifndef IMONITOR_MODULE_H
-#define IMONITOR_MODULE_H
+#ifndef I_MONITOR_MODULE_H
+#define I_MONITOR_MODULE_H
 
 class IMonitorModule
 {
     public:
-        virtual std::string getCurrentTime() = 0;
+        virtual std::string getTime() = 0;
+        virtual std::string getDate() = 0;
         virtual std::string getUserName() = 0;
-        virtual std::string getHostName() = 0; 
+        virtual std::string getHostName() = 0;
+        virtual std::string getOSname() = 0;
+        virtual std::string getProcessorName() = 0;
+        virtual std::string getMemoryDescription() = 0;
+        virtual std::string getGraphics() = 0;
 };
 
 #endif
